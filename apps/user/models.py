2da724cb939b08c93c6c1fdeb456cf78dd66b8c5
bahar <bahar.mehri.7180@gmail.com>
2024-09-06 11:50:46 +0330

    change users model and manager

--- apps/user/models.py
+++ apps/user/models.py
@@ -8,9 +8,11 @@ from apps.user.managers import UserManager
 class User(AbstractUser, BaseModel):
     email = models.EmailField(unique=True)
     number = models.CharField(unique=True, max_length=11, null=True)
-    username = models.CharField(max_length=255, null=True)
+    username = models.CharField(unique=True, max_length=255, null=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username',)
+    USERNAME_FIELD = 'username'
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.username
