d60e37d93d0c7ec1f70402745580ae7526741e22
bahar <bahar.mehri.7180@gmail.com>
2024-09-06 12:21:40 +0330

    use rabbitmq to send otp

--- apps/user/celery_tasks.py
+++ apps/user/celery_tasks.py
@@ -3,9 +3,42 @@ import random
 from celery.app import shared_task
 from django.core.cache import cache
 
+from apps.user.rabbitmq import RabbitSmsOtp, RabbitEmailOtp
 
-# @shared_task
+
+class OTPSender:
+    def send(self, recipient, message):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+
+class EmailSender(OTPSender):
+    def send(self, recipient, message):
+        rabbit_connection = RabbitEmailOtp()
+        rabbit_connection.push({
+            'to': recipient,
+            'body': message
+        })
+
+
+class SMSSender(OTPSender):
+    def send(self, recipient, message):
+        rabbit_connection = RabbitSmsOtp()
+        rabbit_connection.push({
+            'to': recipient,
+            'body': message
+        })
+
+
+def resolve_sender(recipient) -> OTPSender:
+    if '@' in recipient:
+        return EmailSender()
+    return SMSSender()
+
+
+@shared_task
 def send_otp(recipient):
     code = random.randint(100000, 999999)
     cache.set(recipient, code, timeout=60 * 2)
     print(code)
+    sender = resolve_sender(recipient)
+    sender.send(recipient, code)
